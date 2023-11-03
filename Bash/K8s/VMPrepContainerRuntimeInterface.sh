a95e7e9dae6687fc03015615e093058cfeb28f82
ɐɹpuɐɥƆ ʎɹpuǝH <135728071+hendrychandra@users.noreply.github.com>
2023-11-03 16:42:55 +0700

    Update

--- Bash/K8s/VMPrepContainerRuntimeInterface.sh
+++ Bash/K8s/VMPrepContainerRuntimeInterface.sh
@@ -1,19 +1,14 @@
 #!/bin/bash -xe
 
-# ╔════════════════════╗
-# ║   ContainerD.IO    ║
-# ║   Docker-CE        ║
-# ║   Docker-CE-CLI    ║
-# ╚════════════════════╝
+# ╔═══════════════════╗
+# ║   ContainerD.IO   ║
+# ╚═══════════════════╝
 
 sudo echo "Executing $0 $1 $2 $3 $4 $5 $6 $7 $8 $9"
 cd $HOME
 
 # Change the values into empty string '' or "", if you just want to install the latest versions but don't know which version is the latest one.
-# ALL versions must exist. If either one of the above versions does not exist, the whole versions are NOT used at all.
 ContainerDVersion='1.6.20-1'
-DockerCEVersion='5:23.0.2-1~ubuntu.20.04~focal'
-DockerCECLIVersion=$DockerCEVersion
 
 let counter=0;while ( (sudo lsof /var/cache/apt/archives/lock) || (sudo lsof /var/lib/apt/lists/lock) || (sudo lsof /var/lib/dpkg/lock*) || ((`(ps aux -A | grep -i -c "apt")` > 1)) );do let counter++;echo "$counter";if (sudo lsof /var/cache/apt/archives/lock);then printf "$counter sudo lsof /var/cache/apt/archives/lock\n`sudo lsof /var/cache/apt/archives/lock`\n";fi;if (sudo lsof /var/lib/apt/lists/lock);then printf "$counter sudo lsof /var/lib/apt/lists/lock\n`sudo lsof /var/lib/apt/lists/lock`\n";fi;if (sudo lsof /var/lib/dpkg/lock*);then printf "$counter sudo lsof /var/lib/dpkg/lock*\n`sudo lsof /var/lib/dpkg/lock*`\n";fi;if ((`(ps aux -A | grep -i -c "apt")` > 1));then printf "$counter ps aux -A | grep -i \"apt\"\n`ps aux -A | grep -i \"apt\"`\n";fi;sleep 1s;done
 sudo apt update -y
@@ -40,16 +35,16 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 let counter=0;while ( (sudo lsof /var/cache/apt/archives/lock) || (sudo lsof /var/lib/apt/lists/lock) || (sudo lsof /var/lib/dpkg/lock*) || ((`(ps aux -A | grep -i -c "apt")` > 1)) );do let counter++;echo "$counter";if (sudo lsof /var/cache/apt/archives/lock);then printf "$counter sudo lsof /var/cache/apt/archives/lock\n`sudo lsof /var/cache/apt/archives/lock`\n";fi;if (sudo lsof /var/lib/apt/lists/lock);then printf "$counter sudo lsof /var/lib/apt/lists/lock\n`sudo lsof /var/lib/apt/lists/lock`\n";fi;if (sudo lsof /var/lib/dpkg/lock*);then printf "$counter sudo lsof /var/lib/dpkg/lock*\n`sudo lsof /var/lib/dpkg/lock*`\n";fi;if ((`(ps aux -A | grep -i -c "apt")` > 1));then printf "$counter ps aux -A | grep -i \"apt\"\n`ps aux -A | grep -i \"apt\"`\n";fi;sleep 1s;done
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-until ((docker --version | grep -i "docker" | grep -i "version") && (containerd --version | grep -i "containerd")) ; do
+until (containerd --version | grep -i "containerd") ; do
  let counter=0;while ( (sudo lsof /var/cache/apt/archives/lock) || (sudo lsof /var/lib/apt/lists/lock) || (sudo lsof /var/lib/dpkg/lock*) || ((`(ps aux -A | grep -i -c "apt")` > 1)) );do let counter++;echo "$counter";if (sudo lsof /var/cache/apt/archives/lock);then printf "$counter sudo lsof /var/cache/apt/archives/lock\n`sudo lsof /var/cache/apt/archives/lock`\n";fi;if (sudo lsof /var/lib/apt/lists/lock);then printf "$counter sudo lsof /var/lib/apt/lists/lock\n`sudo lsof /var/lib/apt/lists/lock`\n";fi;if (sudo lsof /var/lib/dpkg/lock*);then printf "$counter sudo lsof /var/lib/dpkg/lock*\n`sudo lsof /var/lib/dpkg/lock*`\n";fi;if ((`(ps aux -A | grep -i -c "apt")` > 1));then printf "$counter ps aux -A | grep -i \"apt\"\n`ps aux -A | grep -i \"apt\"`\n";fi;sleep 1s;done
  sudo apt update -y
  
- if [ -z "$ContainerDVersion" ] || [ -z "$DockerCEVersion" ] || [ -z "$DockerCECLIVersion" ] ; then
+ if [ -z "$ContainerDVersion" ] ; then
   let counter=0;while ( (sudo lsof /var/cache/apt/archives/lock) || (sudo lsof /var/lib/apt/lists/lock) || (sudo lsof /var/lib/dpkg/lock*) || ((`(ps aux -A | grep -i -c "apt")` > 1)) );do let counter++;echo "$counter";if (sudo lsof /var/cache/apt/archives/lock);then printf "$counter sudo lsof /var/cache/apt/archives/lock\n`sudo lsof /var/cache/apt/archives/lock`\n";fi;if (sudo lsof /var/lib/apt/lists/lock);then printf "$counter sudo lsof /var/lib/apt/lists/lock\n`sudo lsof /var/lib/apt/lists/lock`\n";fi;if (sudo lsof /var/lib/dpkg/lock*);then printf "$counter sudo lsof /var/lib/dpkg/lock*\n`sudo lsof /var/lib/dpkg/lock*`\n";fi;if ((`(ps aux -A | grep -i -c "apt")` > 1));then printf "$counter ps aux -A | grep -i \"apt\"\n`ps aux -A | grep -i \"apt\"`\n";fi;sleep 1s;done
-  sudo apt install -y containerd.io docker-ce docker-ce-cli
+  sudo apt install -y containerd.io
  else
   let counter=0;while ( (sudo lsof /var/cache/apt/archives/lock) || (sudo lsof /var/lib/apt/lists/lock) || (sudo lsof /var/lib/dpkg/lock*) || ((`(ps aux -A | grep -i -c "apt")` > 1)) );do let counter++;echo "$counter";if (sudo lsof /var/cache/apt/archives/lock);then printf "$counter sudo lsof /var/cache/apt/archives/lock\n`sudo lsof /var/cache/apt/archives/lock`\n";fi;if (sudo lsof /var/lib/apt/lists/lock);then printf "$counter sudo lsof /var/lib/apt/lists/lock\n`sudo lsof /var/lib/apt/lists/lock`\n";fi;if (sudo lsof /var/lib/dpkg/lock*);then printf "$counter sudo lsof /var/lib/dpkg/lock*\n`sudo lsof /var/lib/dpkg/lock*`\n";fi;if ((`(ps aux -A | grep -i -c "apt")` > 1));then printf "$counter ps aux -A | grep -i \"apt\"\n`ps aux -A | grep -i \"apt\"`\n";fi;sleep 1s;done
-  sudo apt install -y containerd.io=$ContainerDVersion docker-ce=$DockerCEVersion docker-ce-cli=$DockerCECLIVersion
+  sudo apt install -y containerd.io=$ContainerDVersion
  fi
 done
 
@@ -57,33 +52,12 @@ sudo mkdir -p /etc/containerd
 sudo containerd config default | sudo tee /etc/containerd/config.toml
 sudo systemctl restart containerd
 
-# sudo sed -i '/^ExecStart/ s/$/ --exec-opt native.cgroupdriver=systemd/' /usr/lib/systemd/system/docker.service
-sudo mkdir -p /etc/systemd/system/docker.service.d
-sudo tee /etc/docker/daemon.json <<EOF
-{
-  "exec-opts": ["native.cgroupdriver=systemd"],
-  "log-driver": "json-file",
-  "log-opts": {
-    "max-size": "22m",
-    "max-file": "333"
-  },
-  "storage-driver": "overlay2"
-}
-EOF
-
-sudo systemctl daemon-reload
-sudo systemctl restart docker
-sudo systemctl enable docker --now
-
 #╔═══════════════════╗
 #║   Review Status   ║
 #╚═══════════════════╝
 
 systemctl status containerd --no-pager
-systemctl status docker --no-pager
 containerd --version
-docker --version
-sudo docker info | grep -i cgroup
 
 #╔═════════════════╗
 #║   Information   ║
@@ -91,12 +65,6 @@ sudo docker info | grep -i cgroup
 
 apt-cache policy containerd.io | grep -i "installed" -B 1
 apt list -a containerd.io | grep -i "installed"
-
-apt-cache policy docker-ce | grep -i "installed" -B 1
-apt list -a docker-ce | grep -i "installed"
-
-apt-cache policy docker-ce-cli | grep -i "installed" -B 1
-apt list -a docker-ce-cli | grep -i "installed"
 
 #╔═════════╗
 #║   End   ║
